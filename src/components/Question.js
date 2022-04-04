3178d09a60055d9380f26c027a19a61daedd1d5e
Emily <emilybribragg@gmail.com>
2022-04-04 09:24:06 -0400

    commit tree 5c8552d42c898ccdccab24d12c89400cee95e50e

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,35 @@
-import React, { useState } from "react";
+/*When the Question component renders, create a side effect using useEffect and use setTimeout 
+to run a callback function after 1 second*/
+
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
-
-  function handleAnswer(isCorrect) {
+  useEffect (() => {
+  if (timeRemaining === 0) {
     setTimeRemaining(10);
-    onAnswered(isCorrect);
+    onAnswered(false);
+    return;
   }
+
+  //initializing timeout to run
+  const timerID = setTimeout(() => {
+    setTimeRemaining((timeRemaining) => timeRemaining - 1)
+  }, 1000);
+
+  //cleanup function for useEffect to clean up after the timeout function: using dependencies timeRemaining, onAnswered
+  return function () {
+    clearTimeout(timerID);
+  };
+}, [timeRemaining, onAnswered]);
+
+//reset timer if question is answered correctly
+function handleAnswer(isCorrect) {
+  setTimeRemaining(10);
+  onAnswered(isCorrect);
+}
 
   const { id, prompt, answers, correctIndex } = question;
 
